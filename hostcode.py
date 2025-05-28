62454679263a36adddb9fc79b94398cfcf908caf
Akash1816 <22btad004@kahedu.edu.in>
2025-05-28 21:29:51 +0530

    project files has been added

---
+++ hostcode.py
@@ -0,0 +1,41 @@
+import streamlit as st
+import time
+
+st.title("***SENTIMENTAL ANALYSIS ON PRODUCT REVIEWS***")
+st.logo(r"D:\project\mini project\archive\DALL·E 2025-03-18 19.00.24 - A modern and futuristic logo for a team named 'HAK'. The design should feature a neon color scheme with vibrant blue, purple, and pink neon glow effec.webp")
+
+st.divider()
+st.header("***About Me***")
+st.write("""Sentiment analysis of product reviews is crucial for understanding customer opinions and improving business strategies. This project utilizes a Random Forest classifier to predict the sentiment of textual reviews. TF-IDF, CountVectorizer, and word embeddings are employed for feature extraction to capture meaningful text representations. The model is trained on labeled review data and evaluated using accuracy, precision, recall, and F1-score. By leveraging ensemble learning, the Random Forest classifier enhances prediction robustness, making it a reliable approach for sentiment classification.
+    """)
+st.divider()
+
+st.subheader("***Insights from the product***")
+st.caption("**Hey there, Initially loading of model would take atleast a minute, meanwhile go through the video below and get to know about our product**")
+st.video(r'D:\project\mini project\archive\Akash_project_1.mp4')
+st.divider()
+
+with st.spinner("Getting the model ready..",show_time=True):
+    import mainmod as md
+    st.success("The model is now trained")       
+st.divider()
+
+     
+st.caption("***feed some text to analyze the sentiments***")
+
+
+text=st.text_input("_shoot the text_","this is a default text")
+predval=md.predictor(text)
+st.write(f"***Sentiment***: :blue[{predval}]")
+
+with st.sidebar:
+    st.sidebar.write("***Jaane se pehle***")
+    g2g=st.sidebar.checkbox("*About my Guru*")
+    if g2g:
+        st.sidebar.caption("***developed by Team HAK***")
+        st.sidebar.caption("***Contact us - hak181318@gmail.com***")
+        
+st.divider()
+    
+
+
---
+++ presentation files for sentimental analysis/main code.py
@@ -0,0 +1,57 @@
+# %%
+import pandas
+import numpy
+import sklearn
+from sklearn.model_selection import train_test_split
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.ensemble import RandomForestClassifier
+import matplotlib.pyplot as plt
+
+df=pandas.read_csv(r'D:\project\mini project\archive\Dataset-SA.csv')
+df.head()                           
+
+sentiment_count=df['Sentiment'].value_counts()
+plt.plot(sentiment_count.index,sentiment_count.values)
+plt.xlabel("Sentiment")
+plt.ylabel("Count")
+plt.title("EDA")
+
+from wordcloud import WordCloud
+text=''.join(df['Summary'].dropna())
+wc=WordCloud(width=640,height=480,background_color='black').generate(text)
+
+plt.figure(figsize=(10,5))
+plt.imshow(wc)
+
+# %%
+df=df.sample(n=50000)
+x=df['Summary'].fillna("")
+y=df['Sentiment'].fillna("")
+x_train,x_test,y_train,y_test=train_test_split(x,y,train_size=0.7,random_state=42)
+
+vec=CountVectorizer()
+X_train=vec.fit_transform(x_train)
+X_test=vec.transform(x_test)
+
+from sklearn.preprocessing import LabelEncoder
+
+enc=LabelEncoder()
+Y_train=enc.fit_transform(y_train)
+Y_test=enc.transform(y_test)
+
+rfc=RandomForestClassifier()
+rfc.fit(X_train,y_train)
+
+# %%
+print(rfc.predict(vec.transform(["hey i love this product"])))
+print(rfc.predict(vec.transform(["the product is not good"])))
+
+# %%
+import pickle
+rfc_model=pickle.dumps(rfc)
+
+# %%
+tm=pickle.loads(rfc_model)
+tm.predict(vec.transform(["this is awesome"]))
+
+
